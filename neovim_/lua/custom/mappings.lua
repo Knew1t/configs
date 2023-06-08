ee8031df3e8d2c6b3ca250f0b2ac242865471f45
Knew1t <longshot@il-k6.msk.21-school.ru>
2023-06-08 14:17:04 +0300

    upload nv chad config

---
+++ neovim_/lua/custom/mappings.lua
@@ -0,0 +1,26 @@
+---@type MappingsTable
+local M = {}
+
+M.disabled = {
+  n = {
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+  },
+  i = {
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+  }
+}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+}
+
+-- more keybinds!
+return M
--- neovim_/lua/user/tmux.lua
+++
@@ -1,4 +0,0 @@
-local status_ok, tmux= pcall(require, "tmux")
-if not status_ok then
-  return
-end
