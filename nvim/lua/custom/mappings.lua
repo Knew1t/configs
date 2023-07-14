a9e206d7757826b3c7d0f7cb720c92011507f3f7
Knew1t <longshot@il-e1.msk.21-school.ru>
2023-07-14 12:52:20 +0300

    mda

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,68 @@
+---@type MappingsTable
+local M = {}
+
+M.disabled = {
+  n = {
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+    ["<A-j>"] = "",
+    ["<A-k>"] = "",
+    ["<A-l>"] = "",
+    -- disable terminal
+    ["<A-i>"] = "",
+    ["<A-h>"] = "",
+    ["<A-v>"] = "",
+    ["<M-i>"] = "",
+    -- disable themes
+    ["<leader>t"] = "",
+    -- disable
+    ["<leader>ff"] = "",
+  },
+  i = {
+    ["<C-b>"] = "",
+    ["<C-e>"] = "",
+    ["<C-h>"] = "",
+    ["<C-l>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+    -- disable terminal
+    ["<A-i"] = "",
+    ["<A-h"] = "",
+    ["<A-v"] = "",
+    ["<A-j>"] = "",
+    ["<A-k>"] = "",
+    ["<A-l>"] = "",
+    ["<leader>n"] = "",
+    ["<leader>rn"] = "",
+  },
+  v = {
+    -- disable terminal
+    ["<A-i"] = "",
+    ["<A-h"] = "",
+    ["<A-v"] = "",
+    ["<A-j>"] = "",
+    ["<A-k>"] = "",
+    ["<A-l>"] = "",
+  },
+  t = {
+    -- disable terminal toggling
+    ["<A-i"] = "",
+    ["<A-h"] = "",
+    ["<A-v"] = "",
+  },
+}
+
+M.general = {
+  n = {
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<C-f>"] = { "<cmd> Telescope find_files <CR>", "Find files" },
+  },
+  v = {
+    ["<leader>ra"] = { '"hy:%s/<C-r>h//g<left><left>' },
+  },
+}
+
+-- more keybinds!
+return M
