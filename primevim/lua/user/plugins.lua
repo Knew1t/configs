ecb70a77827859873cc10da1d9231a3f8a8da49d
Knew1t <happy.fransis@gmail.com>
2023-10-23 03:14:39 +0300

    Update lsp

--- primevim/lua/user/plugins.lua
+++ primevim/lua/user/plugins.lua
@@ -38,5 +38,12 @@ return {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         opts = {} -- this is equalent to setup({}) function
-    }
+    },
+    {
+        "aserowy/tmux.nvim",
+        config = function()
+            return require("tmux").setup()
+        end,
+        lazy = false,
+    },
 }
