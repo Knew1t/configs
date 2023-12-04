c8dd94124d93f89ae7b6acb1508c41f8572e9dea
Knew1t <longshot@ga-a3.msk.21-school.ru>
2023-12-04 15:09:47 +0300

    Add telescope keymaps <C-j,k>

--- primevim/after/plugin/telescope.lua
+++ primevim/after/plugin/telescope.lua
@@ -1,5 +1,25 @@
+local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>r', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+require('telescope').setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = {
+                    actions.move_selection_next,
+                    type = "action",
+                    opts = { nowait = true, silent = true }
+                },
+                ["<C-k>"] = {
+                    actions.move_selection_previous,
+                    type = "action",
+                    opts = { nowait = true, silent = true }
+                },
+            }
+        },
+    },
+})
