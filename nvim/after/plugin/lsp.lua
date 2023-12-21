bbd13d71aa22539b6db88a34c6b7e841d9e12393
Knew1t <happy.fransis@gmail.com>
2023-12-21 21:36:38 +0300

    Removed tab snippet selection

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -84,9 +84,9 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space>'] = cmp.mapping.complete(),
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
+      -- if cmp.visible() then
+      --   cmp.select_next_item()
+      if require("luasnip").expand_or_jumpable() then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
       else
         fallback()
@@ -96,9 +96,9 @@ cmp.setup({
       "s",
     }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
+      -- if cmp.visible() then
+      --   cmp.select_prev_item()
+      if require("luasnip").jumpable(-1) then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
       else
         fallback()
