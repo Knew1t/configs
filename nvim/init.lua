b040a5bf18176f5036d888130d2d95fc2690eea4
Knew1t <longshot@ga-o5.msk.21-school.ru>
2024-01-11 15:05:01 +0300

    Add .tmpl as html

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,8 @@ require("user")
 vim.cmd [[
  au BufRead,BufNewFile *.hbs set filetype=html
 ]]
+vim.cmd [[
+ au BufRead,BufNewFile *.tmpl set filetype=html
+]]
 
 vim.api.nvim_exec([[autocmd BufWritePre *.ts, *.tsx Prettier]], false)
